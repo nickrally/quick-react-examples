bf9f76ef384bb3bf08fb7c5d560b1b3fc22422b3
nickrally <nickrally2009@gmail.com>
2021-03-18 08:57:11 -0600

    currency exchange rate with useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 function App() {
-  const [name, setName] = useState(null);
-  const todos = [
-    'clean bathroom',
-    'wash shower curtain',
-    'remove stains'
-  ]
-  const handleChange = (e) => setName(e.target.value);
+  
+  const [rates, setRates] = useState({})
+  const [base, setBase] = useState('USD')
+
+  const fetchRates = () => {
+    fetch(`https://api.exchangeratesapi.io/latest?base=${base}`)
+      .then(res => res.json())
+      .then(data => setRates(data.rates))
+  }
+  useEffect(() => {
+    fetchRates(base)
+  },[base])
   return (
     <div className="App">
       <div>
-        <label>Name:</label>
-        <input type="text" onChange={handleChange} />
-        {
-          name && <p>Hello, {name}, your todos are:</p>
-        }
-        <div>
-          {todos.map(todo => <li>{todo}</li>)}
-        </div>
+        <h4>useEffect hook</h4>
+        <h2>{base} Exchange Rates</h2>
+        <button onClick={()=>setBase('USD')}>USD</button>
+        <button onClick={()=>setBase('EUR')}>EUR</button>
+        <ul>
+          {
+            Object.keys(rates).map(currency => <li>{rates[currency]}</li>)
+          }
+        </ul>
       </div>
     </div>
   );
