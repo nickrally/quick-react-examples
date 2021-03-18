952dec51f8707714bb881e23ac365df90e8781fb
nickrally <nickrally2009@gmail.com>
2021-03-17 20:59:14 -0600

    diff syntax for setCount

--- src/App.js
+++ src/App.js
@@ -6,15 +6,16 @@ function App() {
   const [count, setCount] = useState(0);
   const [changeBy, setChangeBy] = useState(1)
 
-  const inc = () => setCount(count + changeBy)
-  const dec = () => setCount(count - changeBy)
+  //const inc = () => setCount(count + changeBy)
+  //const dec = () => setCount(count - changeBy)
   return (
     <div className="App">
       <div>
         <h2>Count: {count}</h2>
         <Box setChangeBy={setChangeBy}/>
         <hr/>
-        <Buttons changeBy={changeBy} inc={inc} dec={dec}/>
+        {/*<Buttons changeBy={changeBy} inc={inc} dec={dec}/>*/}
+        <Buttons changeBy={changeBy} setCount={setCount}/>
       </div>
     </div>
   );
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,8 +1,12 @@
-export const Buttons = ({changeBy, inc, dec}) => {
+//export const Buttons = ({changeBy, inc, dec}) => {
+export const Buttons = ({changeBy, setCount}) => {
     return(
-        <>
+        <>  {/*
             <button onClick={()=>inc()}>+ {changeBy}</button>
             <button onClick={()=>dec()}>- {changeBy}</button>
+            */}
+            <button onClick={()=>setCount(i => i + changeBy)}>+ {changeBy}</button>
+            <button onClick={()=>setCount(i => i - changeBy)}>- {changeBy}</button>
         </>
         
     )
